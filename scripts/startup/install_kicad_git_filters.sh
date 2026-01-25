8db8364a04ff3f5a227496bd7ec0afe98b82d880
Jacob Likins <jalikins05@gmail.com>
2026-01-25 15:08:57 -0500

    testing hardware git commit and small updates to Readings

--- scripts/startup/install_kicad_git_filters.sh
+++ scripts/startup/install_kicad_git_filters.sh
@@ -2,3 +2,5 @@
 
 git config --local filter.kicad_sch.clean "sed -E 's/#(PWR|FLG)[0-9]+/#\1?/' "
 git config --local filter.kicad_sch.smudge cat
+
+echo "*.kicad_sch filter=kicad_sch" >> .git/info/attributes
